a56d248a58f31a479a325c4c2c5204c2fb05392d
Kazioslaw <Kamil_d13@wp.pl>
2026-02-28 18:20:25 +0100

    Add Scalar, Configure CORS

--- CountryExplorer.API/Program.cs
+++ CountryExplorer.API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors(cors =>
+{
+	cors.AddPolicy("BlazorUIHTTPS", cors => cors
+	.WithOrigins("https://localhost:7277", "https://countryexplorer-client.dev.localhost:7277")
+	.AllowAnyHeader()
+	.AllowCredentials());
+
+	cors.AddPolicy("BlazorUIHTTP", cors => cors
+	.WithOrigins("http://localhost:5133", "http://countryexplorer-client.dev.localhost:5133")
+	.AllowAnyHeader()
+	.AllowCredentials());
+});
+
 
 var app = builder.Build();
 
@@ -12,6 +27,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
+	app.MapScalarApiReference("/docs");
 }
 
 app.UseHttpsRedirection();
@@ -20,4 +36,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
