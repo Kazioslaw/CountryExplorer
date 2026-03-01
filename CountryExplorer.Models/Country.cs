ecb0d437a0a6b1016b91011a7df9476e8802f891
Kazioslaw <Kamil_d13@wp.pl>
2026-03-01 10:25:19 +0100

    Change models Country and CountryDetails

---
+++ CountryExplorer.Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountryExplorer.Models
+{
+	public class Country
+	{
+		public string FullName { get; set; }
+		public string CommonName { get; set; }
+		public string FlagPath { get; set; }
+		public long Population { get; set; }
+		public string? Capital { get; set; }
+		public string Region { get; set; }
+	}
+}
---
+++ CountryExplorer.Models/CountryDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace CountryExplorer.Models
+{
+	public class CountryDetails
+	{
+		public string FullName { get; set; }
+		public string CommonName { get; set; }
+		public string FlagPath { get; set; }
+		public long Population { get; set; }
+		public string Region { get; set; }
+		public string Subregion { get; set; }
+		public string? Capital { get; set; }
+		public List<string> Currencies { get; set; }
+		public List<string> Languages { get; set; }
+	}
+}
